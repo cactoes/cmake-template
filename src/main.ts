ccb221f1f9b0ed701091976019c54738ada16c8f
cactoes <maartenhvanrooij@gmail.com>
2024-06-30 14:40:56 +0200

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import fs from "fs";
 import fse from "fs-extra";
 import path from "path";
 
-function error(message: string) {
+function error(message: string): number {
     console.error(`Error: ${message}`);
     return 1;
+}
+
+function write_variables(target_path: string | URL, variables: { [key: string]: string }): void {
+    let file = fs.readFileSync(target_path).toString();
+    for (const item of Object.keys(variables))
+        file = file.replaceAll(item, variables[item]);
+    fs.writeFileSync(target_path, file);
 }
 
 function main(argc: number, argv: string[]): number {
@@ -18,33 +25,37 @@ function main(argc: number, argv: string[]): number {
         return 0;
     }
 
+    let project_name = argv[2];
+
+    // for compatibility replace spaces
+    if (project_name.includes(" ")) {
+        console.log("Spaced have been replaced with \"_\"");
+        project_name = project_name.replaceAll(" ", "_");
+    }
+
     const template_dir = path.join(__dirname, "../template");
     const target_path = process.cwd();
     const folder_name = target_path.split("\\").at(-1);
 
+    // this will trigger at drive root (ex. C:\)
     if (!folder_name)
-        return error("Invalid folder");
+        return error("Invalid path");
 
+    // all available variables to use in the template
     const variables = {
         "$FOLDER_NAME": folder_name,
-        "$PROJECT_NAME": argv[2]
+        "$PROJECT_NAME": project_name
     };
 
-    // copy all contents of template
+    // copy the template
     for (const item of fs.readdirSync(template_dir))
         fse.cpSync(path.join(template_dir, item), path.join(target_path, item), { recursive: true });
 
     // replace the vars in the main cmake file
-    let cmake_root_file = fs.readFileSync(path.join(target_path, "CMakeLists.txt")).toString();
-    for (const item of Object.keys(variables))
-        cmake_root_file = cmake_root_file.replaceAll(item, (variables as any)[item]);
-    fs.writeFileSync(path.join(target_path, "CMakeLists.txt"), cmake_root_file);
+    write_variables(path.join(target_path, "CMakeLists.txt"), variables);
 
     // replace the vars in the project file
-    let cmake_project_file = fs.readFileSync(path.join(target_path, "src/$PROJECT_NAME", "CMakeLists.txt")).toString();
-    for (const item of Object.keys(variables))
-        cmake_project_file = cmake_project_file.replaceAll(item, (variables as any)[item]);
-    fs.writeFileSync(path.join(target_path, "src/$PROJECT_NAME", "CMakeLists.txt"), cmake_project_file);
+    write_variables(path.join(target_path, "src/$PROJECT_NAME", "CMakeLists.txt"), variables);
 
     // rename the folder
     fs.renameSync(path.join(target_path, "src/$PROJECT_NAME"), path.join(target_path, `src/${variables["$PROJECT_NAME"]}`));
